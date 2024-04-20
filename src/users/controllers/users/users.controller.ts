2bb4aaaf9518334d28218513b25342363ee15a8a
kresohr <ilicic.kresimir@gmail.com>
2024-04-20 16:36:06 +0200

    Added DTO, class transformer and new controller

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,8 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from './dtos/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
-  getUsers() {
-    return 'users';
+  @Get()
+  getUsers(@Query('sortBy') sortBy: string) {
+    console.log(sortBy);
+    return { username: 'Kreso', email: 'kreso@gmail.com' };
+  }
+
+  @Post('create')
+  @UsePipes(new ValidationPipe())
+  createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return {};
+  }
+
+  @Get('/:id/:postid')
+  getUserById(@Param('id') id: string, @Param('postid') postId: string) {
+    console.log(id);
+    return { id, postId };
   }
 }
